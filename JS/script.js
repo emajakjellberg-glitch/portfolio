21416d00a0bc68aa90d40fdd79776bfef883ce94
Maja Kjellberg <e.maja.kjellberg@gmail.com>
2025-10-25 13:30:15 +0200

    fixes

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.getElementById('hamburger');
   const menu = document.getElementById('menu');
-  const icon = hamburger.querySelector('.material-icons');
 
   hamburger.addEventListener('click', () => {
     menu.classList.toggle('show');
-    icon.textContent = menu.classList.contains('show') ? 'close' : 'menu';
+    hamburger.classList.toggle('active'); // ändrar hamburger till kryss
   });
 });
 
