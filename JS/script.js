ee7e850da7e476d5fc8b897d2580a74e3d4545ef
Maja Kjellberg <e.maja.kjellberg@gmail.com>
2025-10-27 22:13:57 +0100

    chantes

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,12 @@ document.addEventListener('DOMContentLoaded', () => {
   const hamburger = document.getElementById('hamburger');
   const menu = document.getElementById('menu');
 
-  hamburger.addEventListener('click', () => {
+  const toggleMenu = (e) => {
+    e.preventDefault(); 
     menu.classList.toggle('show');
     hamburger.classList.toggle('active');
-  });
+  };
+
+  hamburger.addEventListener('click', toggleMenu);
+  hamburger.addEventListener('touchstart', toggleMenu);
 });
