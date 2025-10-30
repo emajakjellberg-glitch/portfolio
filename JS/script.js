6770c28a10a3837a5c52e94d3d3eda0f1c588d6a
Maja Kjellberg <e.maja.kjellberg@gmail.com>
2025-10-30 17:32:07 +0100

    Menu and h1 on mobile

--- JS/script.js
+++ JS/script.js
@@ -3,11 +3,13 @@ document.addEventListener('DOMContentLoaded', () => {
   const menu = document.getElementById('menu');
 
   const toggleMenu = (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
     menu.classList.toggle('show');
     hamburger.classList.toggle('active');
+    const icon = hamburger.querySelector('i');
+    icon.classList.toggle('fa-bars');
+    icon.classList.toggle('fa-xmark');
   };
 
   hamburger.addEventListener('click', toggleMenu);
-  hamburger.addEventListener('touchstart', toggleMenu);
 });
